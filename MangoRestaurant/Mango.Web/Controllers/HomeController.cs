cb0e8aacc2ac40e962584e4fb662bbb6ed0fb4b5
Paul Villalobos <wilverpaul@gmail.com>
2022-04-19 18:42:04 -0500

    Home and Details view completed

--- MangoRestaurant/Mango.Web/Controllers/HomeController.cs
+++ MangoRestaurant/Mango.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
@@ -7,15 +9,38 @@ namespace Mango.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto> products = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(products);
+        }
+
+        public async Task<IActionResult> Details(int productId)
+        {
+            ProductDto model = new();
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+
+            return View(model);
         }
 
         public IActionResult Privacy()
